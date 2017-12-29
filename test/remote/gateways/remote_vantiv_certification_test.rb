70d3ee964994932f5a13bdd724a3e1b3720eb39e
Grant Bourque <grant@anedot.com>
2017-12-29 16:16:45 -0600

    Vantiv: Update certification tests
    - Update expectations to match the newer certification test responses
      so that the certification tests pass.

--- test/remote/gateways/remote_vantiv_certification_test.rb
+++ test/remote/gateways/remote_vantiv_certification_test.rb
@@ -250,7 +250,7 @@ class RemoteVantivCertification < Test::Unit::TestCase
     response = @gateway.void(response.authorization, {order_id: "6A"})
 
     assert_equal "360", response.params["response"]
-    assert_equal "No transaction found with specified litleTxnId", response.message
+    assert_equal "No transaction found with specified transaction Id", response.message
   end
 
   def test7
@@ -816,13 +816,13 @@ class RemoteVantivCertification < Test::Unit::TestCase
     credit_response = @gateway.refund(nil, authorization, options)
 
     assert_equal "360", credit_response.params["response"]
-    assert_equal "No transaction found with specified litleTxnId", credit_response.params["message"]
+    assert_equal "No transaction found with specified transaction Id", credit_response.params["message"]
 
     # eCheck Void transaction test
     void_response = @gateway.void(authorization)
 
     assert_equal "360", void_response.params["response"]
-    assert_equal "No transaction found with specified litleTxnId", void_response.params["message"]
+    assert_equal "No transaction found with specified transaction Id", void_response.params["message"]
   end
 
   ### Order Ids 50 through 52 - Explicit card tokenization certification tests
